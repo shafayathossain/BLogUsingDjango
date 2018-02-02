0be8d1d27bc282e576c15cb20fd3ac611bdd5d15
shafayat <shafayathossainkhan@gmail.com>
2018-02-02 10:35:12 +0600

    post urls mapping

---
+++ posts/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import url
+from posts import views
+
+urlpatterns = [
+    url(r'^create$', views.post_create),
+    url(r'^detail$', views.post_detail),
+    url(r'^list$', views.post_list),
+    url(r'^update$', views.post_update),
+    url(r'^delete$', views.post_delete),
+
+]
--- posts/views.py
+++ posts/views.py
@@ -2,5 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def post_home(request):
-    return HttpResponse("<h1>hello</h1>")
+def post_create(request):
+    return HttpResponse("<h1>create</h1>")
+def post_detail(request):
+    return HttpResponse("<h1>detail</h1>")
+def post_list(request):
+    return HttpResponse("<h1>list</h1>")
+def post_update(request):
+    return HttpResponse("<h1>update</h1>")
+def post_delete(request):
+    return HttpResponse("<h1>delete</h1>")
