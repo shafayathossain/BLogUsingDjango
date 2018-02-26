3cf63ce6695b1b24507962e01dc08f8938650167
shafayat <shafayathossainkhan@gmail.com>
2018-02-26 21:07:21 +0600

    query to get list and get item or 404 in detail added

--- posts/views.py
+++ posts/views.py
@@ -1,18 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-
+from .models import Post
 # Create your views here.
 def post_create(request):
     return render(request, "index.html", {})
     #return HttpResponse("<h1>create</h1>")
 def post_detail(request):
-    context = {'title':'detail'}
-    return render(request, "index.html", context)
+    instance = get_object_or_404(Post, id = 1)
+    context = {
+        'title':'detail',
+        'obj':instance
+    }
+    return render(request, "post_detail.html", context)
 def post_list(request):
-    if request.user.is_authenticated():
-        context = {'title':'user list'}
-    else:
-        context = {'title':'list'}
+    queryset = Post.objects.all()
+    context = {
+        'title':'list',
+        'object_list':queryset
+        }
     return render(request, "index.html", context)
 def post_update(request):
     return HttpResponse("<h1>update</h1>")
