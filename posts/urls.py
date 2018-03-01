042151efc9991b4af0be71c35e3a14ef7ebba50d
shafayat <shafayathossainkhan@gmail.com>
2018-03-01 22:37:44 +0600

    template inheritate

--- posts/urls.py
+++ posts/urls.py
@@ -4,8 +4,8 @@ from posts import views
 urlpatterns = [
     url(r'^create$', views.post_create),
     url(r'^(?P<id>\d+)/$', views.post_detail, name="detail"),
-    url(r'^$', views.post_list),
-    url(r'^(?P<id>\d+)/edit$', views.post_update),
-    url(r'^delete$', views.post_delete),
+    url(r'^$', views.post_list, name='list'),
+    url(r'^(?P<id>\d+)/edit$', views.post_update, name='update'),
+    url(r'^(?P<id>\d+)/delete$', views.post_delete),
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from .models import Post
 from .forms import PostForm
@@ -28,7 +28,7 @@ def post_list(request):
         'title':'list',
         'object_list':queryset
         }
-    return render(request, "index.html", context)
+    return render(request, "base.html", context)
 def post_update(request, id):
     instance = get_object_or_404(Post, id = id)
     form = PostForm(request.POST or None, instance=instance)
@@ -40,7 +40,9 @@ def post_update(request, id):
         'form':form
     }
     return render(request, "post_form.html", context)
-def post_delete(request):
-    return HttpResponse("<h1>delete</h1>")
+def post_delete(request, id):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    return redirect("posts:list")
 def post_home():
     return HttpResponse("<h1>hello</h1>")
